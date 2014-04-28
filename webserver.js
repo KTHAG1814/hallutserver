f1d37824fcfc32f0bbbfbeb466b81aeefc9d3aa0
Setfal <perlinman@gmail.com>
2014-04-28 19:51:17 +0200

    added code in webserver.js using koa and koa-views templating that now allows it to serve content to users.

--- webserver.js
+++ webserver.js
@@ -1,3 +1,40 @@
+var db = require('rethinkdb');
+var nconf = require('nconf');
+var thunkify = require('thunkify');
+var koa = require('koa');
+var views = require('koa-views');
+
+nconf.file({ file: 'config.json' });
+
+db.connect = thunkify(db.connect);
+
 exports.start = function* () {
-	
-}
+	var app = koa();
+
+	app.use(views({
+		cache: true,
+		map: {
+			html: 'underscore'
+		}
+	}));
+
+	app.use(function*(next) {
+		console.log('Got request.');
+		yield next;
+		console.log('Finished request.');
+	});
+
+	app.use(function*(next) {
+		if(this.path === '/' || this.path === '/data.html') {
+			yield this.render('index', {
+				content: '<h1>Works.</h1>'
+			});
+		} else {
+			yield this.render('404', {
+				path: this.path
+			});
+		}
+	});
+
+	app.listen(nconf.get('webserver:port'));
+};
